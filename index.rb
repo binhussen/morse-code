148f5a0227d2a2414e8ab6e4c3f788122a03a5da
binhussen <mhdhsna@gmail.com>
2022-10-27 11:26:16 +0300

    Add reusable def for word and message

--- index.rb
+++ index.rb
@@ -8,3 +8,15 @@ def decode_char(char)
 
   morse_code[char]
 end
+
+def decode_word(word)
+  word_char = word.split
+  word_char.map { |char| decode_char(char) }.join
+end
+
+def decode_message(message)
+  message_word = message.split('   ')
+  message_word.map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
